e55eba2aa8f9ae4d645e8af0dd0970fbfe624013
toro-sal <113482574+toro-sal@users.noreply.github.com>
2024-01-28 18:00:37 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <omp.h>
+
+#define N 1000
+#define chunk 100
+#define mostrar 10
+
+void imprimeArreglo(float* d);
+int main()
+{
+	std::cout << "Sumando Arreglos en Paralelo!/n";
+	float a[N], b[N], c[N];
+	int i;
+
+	for (i = 0; i < N; i++)
+	{
+		a[i] = i * 10;
+		b[i] = (i + 3) * 8.2;
+	}
+	int pedazos = chunk;
+
+	#pragma omp parallel for \
+	shared(a,b,c,pedazos) private(i) \
+	schedule(static,pedazos)
+
+	for (i = 0; i < N; i++)
+		c[i] = a[i] + b[i];
+	std::cout << "Imprimiendo los primeros" << mostrar << "valores del arreglo a: " << std::endl;
+	imprimeArreglo(a);
+	std::cout << "Imprimiendo los primeros" << mostrar << "valores del arreglo b: " << std::endl;
+	imprimeArreglo(b);
+	std::cout << "Imprimiendo los primeros" << mostrar << "valores del arreglo c: " << std::endl;
+	imprimeArreglo(c);
+
+}
+void imprimeArreglo(float* d)
+{
+	for (int x = 0;x < mostrar;x++)
+		std::cout << d[x] << " - ";
+	std::cout << std::endl;
+}
